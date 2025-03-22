db89062aba4d5d9d26f688761b07e97593f2c0b7
Andrew Thul <thulio63@gmail.com>
2025-03-22 16:51:16 -0500

    introduced dynamic data, assigned id to data before rendering

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,10 +1,41 @@
 import "./App.css";
 
+function Header({ name }) {
+  return (
+    <header>
+      <h1>It's {name} Time!!!</h1>
+      <h2>lets get toolin'</h2>
+    </header>
+  );
+}
+
+const items = ["Smokin' Aces", "Iron & Ink", "Illinois Tattoo Co."];
+
+const parlorMap = items.map((location, i) => ({
+  id: i,
+  location: location,
+}));
+
+function Main({ parlor }) {
+  return (
+    <main>
+      <ul>
+        {parlor.map((parlor) => (
+          <li key={parlor.id} style={{ listStyleType: "none" }}>
+            {parlor.location}
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
+
 function App() {
   return (
-    <>
-      <h1>It's Thul Time!</h1>
-    </>
+    <div>
+      <Header name="Thul" />
+      <Main year={new Date().getFullYear()} parlor={parlorMap} />
+    </div>
   );
 }
 
